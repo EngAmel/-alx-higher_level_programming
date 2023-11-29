aae57dcb625ffcdfbde247a2d864aff8b5105439
EngAmel <amel.abdululazim.se@gmail.com>
2023-11-29 03:38:14 +0300

    task

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 def uppercase(str):
-    if ord(*str) in range(ord('a'), ord('z') + 1):
-        ord(*str) = ord(*str) - 32
-    print("{:s}.format(*str)")
+    for s in str:
+        if ord(s) in range(ord('a'), ord('z') + 1):
+            ord(s) = ord(s) - 32
+    print("{:c}.format(s)", end="")

dce36717b691647238d4f63161dca5ff94f9568d
EngAmel <amel.abdululazim.se@gmail.com>
2023-11-29 05:35:30 +0300

    task

---
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+new = ""
+for s in range(ord('z'), ord('a') - 1, -1):
+    if (ord('z') - s) % 2 == 0:
+        new += chr(s)
+    else:
+        new += chr(s - 32)
+print("{:s}".format(new))

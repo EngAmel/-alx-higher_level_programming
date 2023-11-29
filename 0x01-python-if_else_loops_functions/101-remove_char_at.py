fc23fd5b554e111591679588e2e7cd822d8a02bf
EngAmel <amel.abdululazim.se@gmail.com>
2023-11-29 05:47:18 +0300

    task

---
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def remove_char_at(str, n):
+    new = ""
+    for ch in str:
+        new += chr(s)
+        if ch == n:
+            continue
+    print("{:s}".format(new), end="")

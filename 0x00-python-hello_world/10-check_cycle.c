b0d5fdba3cee5b5e4e9b5872c3539c6561ea50c0
EngAmel <amel.abdululazim.se@gmail.com>
2023-11-30 04:23:26 +0300

    task

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -1,8 +1,20 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include <lists.h>
-
-int int check_cycle(listint_t *list)
+/**
+ * check_cycle - function check cycle
+ * @list:node
+ *
+ * Return: 1 if there cycle 0 if not.
+ **/
+int check_cycle(listint_t *list)
 {
-	listint_t *slow =list,
+	listint_t *slow = list, *first = list;
+
+		while (slow != NULL && first != NULL)
+		{
+			slow = slow->next;
+			first = first->next;
+			if (slow == first)
+				return (1);
+		}
+		return (0);
 }

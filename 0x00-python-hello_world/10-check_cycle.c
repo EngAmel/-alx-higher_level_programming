ac6ea067b6427a9e5833d0de4025e76ee18d791f
EngAmel <amel.abdululazim.se@gmail.com>
2023-11-30 04:53:38 +0300

    task

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -12,7 +12,7 @@ int check_cycle(listint_t *list)
 {
 	listint_t *slow = list, *first = list;
 
-		while (list != NULL && slow != NULL && first != NULL)
+		while (slow != NULL && first != NULL && first->next != NULL)
 		{
 			slow = slow->next;
 			first = first->next->next;

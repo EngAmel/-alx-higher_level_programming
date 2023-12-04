fda5f26a654120d1f4d2f24fc1d169fb4f0c8b5e
EngAmel <amel.abdululazim.se@gmail.com>
2023-12-05 02:36:49 +0300

    task

---
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -0,0 +1,10 @@
+def replace_in_list(my_list, idx, element):
+    j = 0
+    if idx > len(my_list) or idx < 0:
+        return my_list
+    else:
+        for i in my_list:
+            if idx == j:
+                my_list[j] = element
+                return my_list
+            j += 1

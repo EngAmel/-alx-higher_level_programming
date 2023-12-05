8b937a42905fbe84f2907007f59f05d379e68b90
EngAmel <amel.abdululazim.se@gmail.com>
2023-12-05 05:55:20 +0300

    task

---
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -0,0 +1,23 @@
+#include <Python.h>
+
+/**
+ *print_python_list_info - print basic info
+ *@p: The list
+ */
+
+void print_python_list_info(PyObject *p)
+{
+	int si, loc, count;
+	Pyobject *obj;
+
+	size = Py_SIZE(p);
+	loc = ((PyListobject *)p)->allocated;
+	printf("[*] Size of the Python List = %d\n", si);
+	printf("[*] Allocated = %d\n", loc);
+	for (count = 0; count < si; count++)
+	{
+		printf("Element %d: ", loc);
+		obj = PyList_GetItem(p, count);
+		printf("%s\n", py_TYPE(obj)->tp_name);
+	}
+}

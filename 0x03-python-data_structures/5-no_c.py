420285ca0e7c2990f43d11b65108e9b49f58fc8c
EngAmel <amel.abdululazim.se@gmail.com>
2023-12-05 05:17:08 +0300

    task

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def no_c(my_string):
+    i = 0
+    new = ""
+    for ch in my_string:
+        if ch == 'c' or ch == 'C':
+            continue
+        else:
+            new += ch
+        i += 1
+    print(new)

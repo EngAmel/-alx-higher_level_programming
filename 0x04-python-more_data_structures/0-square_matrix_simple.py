13e4fba76f829c2d0192818f32573270983eafdb
EngAmel <amel.abdululazim.se@gmail.com>
2023-12-07 01:42:43 +0300

    task

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    new = [row[:] for row in matrix]
-    for i in range(len(new)):
-        for j in range(len(new[i])):
-            new[i][j] = new[i][j] ** 2
-        return new
+   new = [[j ** 2 for j in i] for i in matrix]
+   return new

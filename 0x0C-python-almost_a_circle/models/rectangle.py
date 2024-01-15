3ea62c5390939336f50db1a8583d84f0dcc5ffbb
EngAmel <amel.abdululazim.se@gmail.com>
2024-01-16 00:41:15 +0300

    task

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -17,44 +17,35 @@ class Rectangle(Base):
     @property
     def width(self):
         '''__width -> width'''
-
         return self.__width
 
     @width.setter
     def width(self, value):
-        '''width setter'''
-    
         self.__width = value
 
     @property
     def height(self):
         '''height -> height'''
-
         return self.__height
 
     @height.setter
     def height(self, value):
-        '''height setter'''
         self.__height = value
 
     @property
     def x(self):
         '''__x -> x'''
-
         return self.__x
 
     @x.setter
     def x(self, value):
-        '''x setter'''
         self.__x = value
 
     @property
     def y(self):
         '''y -> y'''
-
         return self.__y
 
     @y.setter
     def y(self, value):
-        '''y setter'''
         self.__y = value

f74d381f04befd2ca4089a6f2212e47b06f183b4
EngAmel <amel.abdululazim.se@gmail.com>
2024-01-16 01:00:41 +0300

    task

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,10 +1,13 @@
 #!usr/bin/python3
-from models.base import Base
 '''rectangle module'''
+
+
+from models.base import Base
 
 
 class Rectangle(Base):
     '''rectangle class'''
+
 
     def __init__(self, width, height, x=0, y=0, id=None):
         '''constructor'''

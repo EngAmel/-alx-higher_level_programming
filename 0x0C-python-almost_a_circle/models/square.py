7280ab8d2659f7782b1b619ec14530d4267137cb
EngAmel <amel.abdululazim.se@gmail.com>
2024-01-16 05:49:03 +0300

    task

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -18,16 +18,36 @@ class Square(Rectangle):
 
         return '[{}] ({}) {}/{} - {}'.\
             format(type(self).__name__, self.id, self.x, self.y, self.width)
-    
+
     @property
     def size(self):
         '''getter Size'''
-        
+
         return self.width
 
     @size.setter
     def size(self, value):
         '''setter'''
-    
+
         self.width = value
         self.height = value
+
+    def __update(self, id=None, size=None, x=None, y=None):
+        '''update args'''
+
+        if id:
+            self.id = id
+        if size:
+            self.size = size
+        if x:
+            self.x = x
+        if y:
+            self.y = y
+
+    def update(self, *args, **kwargs):
+        '''update kwargs.'''
+
+        if args:
+            self.__update(*args)
+        elif kwargs:
+            self.__update(**kwargs)
